96b5c333cadc142e2fef99e4a3f6b43a18c40cd6
Oriol Canalias <s2it_ocanalias@uolinc.com>
2018-03-06 16:30:30 -0300

    tirando o out

--- basic/src/main/java/br/com/devcave/transactional/basic/domain/User.java
+++ basic/src/main/java/br/com/devcave/transactional/basic/domain/User.java
@@ -1,16 +1,18 @@
 package br.com.devcave.transactional.basic.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Data
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"document"}))
 @NoArgsConstructor
 public class User {
 
@@ -20,6 +22,7 @@ public class User {
 
     private String name;
 
+    @Column(unique = true)
     private String document;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
